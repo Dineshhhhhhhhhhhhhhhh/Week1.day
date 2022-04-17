6417009355cd02dd88b53eef57c279342c104908
Dineshhhhhhhhhhhhhhhh <103868573+Dineshhhhhhhhhhhhhhhh@users.noreply.github.com>
2022-04-17 08:55:42 +0530

    Add files via upload

---
+++ ConversionPositiveNumber.java
@@ -0,0 +1,17 @@
+package Week1.day1;
+
+public class ConversionPositiveNumber {
+	public static void main(String[] args) {
+		int r = -20;
+		int s;
+		
+		if (r<0)
+		{
+			s = r * (-1);
+			System.out.println("The conversion of negative number to positive number is "+s);
+			
+		}
+			
+	}
+}
+
---
+++ PositiveOrNegative.java
@@ -0,0 +1,22 @@
+package Week1.day1;
+
+public class PositiveOrNegative {
+	public static void main(String[] args) {
+		int a = -20;
+		
+		int b = 20;
+		
+		if(a < 0)
+			System.out.println(a+ " is a negative number");
+		else
+			System.out.println(a+ " is a positive number");
+	
+		
+		if (b < 0)
+			System.out.println(b+ " is a negative number");
+		else
+			System.out.println(b+ " is a positive number");
+	}
+
+}
+
